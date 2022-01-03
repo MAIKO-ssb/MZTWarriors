409ab4b81689e3d350f81a1a569c13ab6da147df
MAIKO <maiko.ssb@gmail.com>
2022-01-03 10:37:50 -0800

    copy

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to The Manzanita Tribe.
+          Welcome to The Manzanita Tribe!
         </h1>
       </main>
     </div>
