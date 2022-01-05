53dc3682efc9fab5b904c1cb1a9e56750b4f3ffa
MAIKO <maiko.ssb@gmail.com>
2022-01-05 14:10:10 -0800

    SASS and Landing

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,44 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Manzanita Tribe</title>
         <meta name="description" content="Manzanita Tribe" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to The Manzanita Tribe!
-        </h1>
-      </main>
+      
+      
+      <div className='s-hero'>
+        <main>
+          {/* MAIN CONTENT */}
+          <div className='u-flex u-flex1'>
+            <div className='mzt-logo u-animated a-fadeIn'>
+              <Image className='mzt-logo__img' width="1490" height="1490" src="/img/mzt-logo.png" alt="MZT Warriors Logo" />
+              
+            </div>
+          </div>
+          <div className='u-flex u-flex1'>
+            <div>
+              <h1>MZT Warrior Badge</h1>
+              <p>Lorem Ipsum Dolor Sit Amet Consecetur...</p>
+              <button>Mint a 'Warrior Badge'</button>
+            </div>
+          </div>
+          
+          {/* <div style={{position:"absolute", bottom: "0", display:"block", transform: "translateY(100px)"}}>
+            <Image layout="intrinsic" width="1920" height="340" src="/img/tribal-ground.svg" alt="" style={{display:"block"}}/>
+          </div> */}
+        </main>
+        <video width="100%" height="auto" autoPlay muted playsInline loop>
+          <source src="/video/starry-night-bg.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+      
+      
     </div>
   )
 }
