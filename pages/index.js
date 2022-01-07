fe9b2220bcd869f42b5ecd8b0547cbfdf0199338
MAIKO <maiko.ssb@gmail.com>
2022-01-07 13:50:30 -0800

    Team & Contact

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useState } from 'react'
+
 
 export default function Home() {
+
+  const [contentView, setContentView] = useState('mint')
+
   return (
     <div>
       <Head>
@@ -19,42 +24,135 @@ export default function Home() {
               <Image className='mzt-logo__img u-animated a-fadeIn' width="1490" height="1490" src="/img/mzt-logo.png" alt="MZT Warriors Logo" />
             </div>
           </div>
-          <div className='u-flex u-flex1' style={{padding: "1.5em"}}>
+          <div className='u-flex u-flex1' style={{padding: "1.5em", flexDirection:"column", minHeight: "720px", width:"100%"}}>
+            <div className='c-menu'>
+              <ul className='u-list-unstyled'>
+                <li>
+                  <span className={`${contentView == 'mint' ? '-active': ''}`} onClick={() => setContentView('mint')}>
+                    Mint NFT 
+                  </span>
+                </li>
+                <li>
+                  <span className={`${contentView == 'bios' ? '-active': ''}`} onClick={() => setContentView('bios')}>
+                    The Team 
+                  </span>
+                </li>
+                <li>
+                  <span className={`${contentView == 'contact' ? '-active': ''}`} onClick={() => setContentView('contact')}>
+                    Contact
+                  </span>
+                </li>
+              </ul>
+            </div>
             <div style={{maxWidth:"650px"}}>
-              <h1>The MZT Warrior Badge (2022)</h1>
-              <p>
-                The `Manzanita Tribe Warriors` is a small esports team located in Southern California. 
-              </p>
-              <p> 
-                We have competed in `Super Smash Bros. Ultimate` for a few years now, and we want to be able continue our journey as a team, competing &amp; pushing our limits this 2022🔥! 
-              </p>
-              <p>
-                As a <strong>team fundraising</strong> effort, we are dropping a limited edition of 1000 digital `Warrior Badges` priced at .1 solana each.</p>
-              <p>
-                We`d like to give special thanks to all of the competitive smash bros. community and everyone that follows us, the Manzanita Tribe Warriors ᴹᶻᵀ.
-              </p>
-              
-              
-              <button>Mint a `Warrior Badge`*</button>
-              <br/>
-              <h3>1,000 / 1,000 available</h3>
-              <h4>Price: .1 sol</h4>
-              <br/>
-              <small>
-                * Your support of .1 sol goes directly to our team`s wallet, which helps fund our costs such as team equipment, player wages, event entries, transportation, hotel, food, etc. 
-                All re-sales include a 10% royalty fee that directly supports the team.
-                This team badge serves as a representation of your support.   
-                Thank you.
-              </small>
-              <br/>
-              <div style={{marginTop:"1em"}}>
-                <h4> 
-                  Our next competition:
-                </h4>
-                <p>
-                  1/28 - San Jose, CA - Major Event - <a href="https://smash.gg/tournament/genesis-8/details">Genesis 8</a> 
-                </p>
-              </div>
+              { 
+                contentView == 'mint' ?
+                <>
+                  <h1>The MZT Warrior Badge (2022)</h1>
+                  <p>
+                    The `Manzanita Tribe Warriors` is a small esports team located in Southern California. 
+                  </p>
+                  <p> 
+                    We have competed in `Super Smash Bros. Ultimate` for a few years now and we want to be able continue our journey as a team, competing &amp; pushing our limits this 2022🔥! 
+                  </p>
+                  <p>
+                    As a <strong>team fundraising</strong> effort, we are dropping a limited edition of 1000 digital `Warrior Badges` priced at .1 solana each.</p>
+                  <p>
+                    We`d like to give special thanks to all of the competitive smash bros. community and everyone that follows and supports us, the Manzanita Tribe Warriors ᴹᶻᵀ
+                  </p>
+                  
+                  
+                  <button>Mint `ᴹᶻᵀ Warrior Badge`*</button>
+                  <br/>
+                  <h3>1,000 / 1,000 available</h3>
+                  <h4>Price: .1 sol</h4>
+                  <br/>
+                  <small>
+                    * Your support of .1 sol goes directly to our team`s wallet, which helps fund our costs such as team equipment, player wages, event entries, transportation, hotel, food, etc. 
+                    All re-sales include a 10% royalty fee that directly supports the team.
+                    This team badge serves as a representation of your support.   
+                    Thank you.
+                  </small>
+                  <br/>
+                  <div style={{marginTop:"1em"}}>
+                    <h4> 
+                      Our next competition:
+                    </h4>
+                    <p>
+                      1/28 - San Jose, CA - Major Event - <a href="https://smash.gg/tournament/genesis-8/details">Genesis 8</a> 
+                    </p>
+                  </div>
+                </>
+                  
+                : contentView == 'bios' ?
+                  <>
+                    <h1>About the Team</h1>
+                    <div className='mzt-bio u-flex'>
+                      <div className='mzt-bio__imgWrap'>
+                        <Image className='mzt-bio__img' width="150" height="150" src="/img/pfpMaiko.jpg" alt="MAIKO Profile Picture" />
+                      </div>
+                      <div className='mzt-bio__content'>
+                        <h2>&lt;MAIKO/&gt;</h2>
+                        <em>Founder</em>
+                        <p>
+                          Twitter: &nbsp;
+                          <a href="https://twitter.com/maiko_ssb" target="_blank" rel="noreferrer" alt="Twitter Link">@maiko_ssb</a>
+                        </p>
+                      </div>
+                    </div>
+                    <div className='mzt-bio u-flex'>
+                      <div className='mzt-bio__imgWrap'>
+                        <Image className='mzt-bio__img' width="150" height="150" src="/img/pfpSin.png" alt="Sin Profile Picture" />
+                      </div>
+                      <div className='mzt-bio__content'>
+                        <h2>Sin</h2>
+                        <em>Co-Founder</em>
+                        <p>
+                          Twitter: &nbsp;
+                          <a href="https://twitter.com/Sin_Puff" target="_blank" rel="noreferrer" alt="Twitter Link">@sin_puff</a>
+                        </p>
+                      </div>
+                    </div>
+                    <div className='mzt-bio u-flex'>
+                      <div className='mzt-bio__imgWrap'>
+                        <Image className='mzt-bio__img' width="150" height="150" src="/img/pfpTai.jpg" alt="Tai Profile Picture" />
+                      </div>
+                      <div className='mzt-bio__content'>
+                        <h2>Tai</h2>
+                        <em>Co-Founder</em>
+                        <p>
+                          Twitter: &nbsp;
+                          <a href="https://twitter.com/ssbtai" target="_blank" rel="noreferrer" alt="Twitter Link">@ssbtai</a>
+                        </p>
+                      </div>
+                    </div>
+                  </>
+                : contentView == 'contact' ?
+                  <>
+                    <h1>Contact Us</h1>
+                    <p>
+                      Email: &nbsp; 
+                      <a href="mailto:mztwarriors@gmail.com" target="_blank" rel="noreferrer">MZTWarriors@gmail.com</a>
+                    </p>
+                    <p>
+                      Twitter: &nbsp;
+                      <a href="https://twitter.com/mzt_warriors" target="_blank" rel="noreferrer" alt="Twitter Link">@MZT_Warriors</a>
+                    </p>
+                    <p>
+                      Twitch: &nbsp;
+                      <a href="https://twitch.com/mztwarriors" target="_blank" rel="noreferrer" alt="Twitch Link">@MZTWarriors</a>
+                    </p>
+                    <p>
+                      Instagram: &nbsp;
+                      <a href="https://www.instagram.com/manzanita_tribe/" target="_blank" rel="noreferrer" alt="Instagram Link">@manzanita_tribe</a>
+                    </p>
+                    <p>
+                      Solana Wallet: &nbsp; 
+                      🍎🔥.sol
+                    </p>
+                  </>
+                : null
+              }
             </div>
           </div>
           
