e7fa6bc0bab6ee4471c4b652c68ceaad2f88fd63
Pamela Monroy <maiko.ssb@gmail.com>
2025-07-26 10:05:00 -0700

    Removed mint button

--- pages/index.js
+++ pages/index.js
@@ -500,7 +500,7 @@ try {
             </div>
             
             {/* MINT BUTTON AND WALLET CONNECTION */}
-            {web3JsWalletPublicKey ? (
+            {/* {web3JsWalletPublicKey ? (
               <div style={{ marginTop: '20px', textAlign: 'center' }}>
                 <WalletMultiButton />              
                 <p style={{ marginBottom: '10px', color: 'white',  wordBreak: 'break-all', overflowWrap: 'anywhere'}}>
@@ -599,7 +599,7 @@ try {
                 <WalletMultiButton />
                 <p style={{ color: 'white', marginTop: '10px' }}>Connect your Solana wallet to mint a Warrior.</p>
               </div>
-            )}
+            )} */}
             
             <div style={{display: 'flex', justifyContent: 'center', marginTop: '1em'}}>
               <Image src="/img/solanaLogo.png" alt="Solana Network" width={100} height={15} />
